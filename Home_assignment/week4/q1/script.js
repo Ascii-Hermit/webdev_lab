cbfbbfa12a8d625dca9204c071961ffaa47cfbb6
chris <christiem802@gmail.com>
2025-02-08 14:25:21 +0530

    converted to jQuery

--- Home_assignment/week4/q1/script.js
+++ Home_assignment/week4/q1/script.js
@@ -1,9 +1,20 @@
-$(document).ready(function () {
-  function bounce() {
-    $(".ball")
-      .animate({ top: "350px" }, 1000)
-      .animate({ top: "0px" }, 1000, bounce);
-  }
+// $(document).ready(function () {
+//   function bounce() {
+//     $(".ball")
+//       .animate({ top: "350px" }, 1000)
+//       .animate({ top: "0px" }, 1000, bounce);
+//   }
 
+//   bounce();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  function bounce() {
+    let ball = document.querySelector(".ball");
+    ball.animate([{ top: "0px" }, { top: "350px" }, { top: "0px" }], {
+      duration: 2000,
+      iterations: Infinity,
+    });
+  }
   bounce();
 });
